be0f1f6996913d5d629886d82ebd4fcdc83c3bcd
sinan <sinan1987147904@dingtalk.com>
2023-05-15 13:24:27 +0800

    feat:获取数据字段值

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ fs.readFile("./domo.html", "utf8", (err, res) => {
   if (err) return console.log(err);
 
   const reg = /data: ({[\s\S]*}),\n\s*methods/;
-  const result = res.match(reg);
-  console.log("拿到数据了", JSON.parse(result && result[1]));
+  const matchRes = res.match(reg);
+
+  let obj = JSON.parse(matchRes[1]);
+  // console.log("拿到数据了", obj);
+
+  // 余额 - balance
+  let balance = obj.dashboard.value;
+  console.log("余额:", balance);
+
+  // 表号 - electricMeter
+  let electricMeter = obj.dashboard.items[0].value.slice(0, 12);
+  console.log("表号:", electricMeter);
+
+  // 采集日期 - acquisitionTime 
+  let acquisitionTime = obj.dashboard.items[1].value;
+  console.log("采集日期:", acquisitionTime);
+
+  // 户名 - accountName
+  let accountName = obj.dashboard.items[2].value;
+  console.log("户名:", accountName);
+
+  // 上次缴费日期 - lastChargeDate
+  let lastChargeDate = obj.payment.time + '日';
+  console.log("上次缴费日期:", lastChargeDate);
+
+  // 上次缴费金额 - lastChargeAmount
+  let lastChargeAmount = obj.payment.opration.replace(/[^0-9|\.]/g, '');
+  console.log("上次缴费金额:", lastChargeAmount);
 });
