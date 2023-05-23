3ffc6829fc8a16a71a8ff1500cef9b66056c0567
STDSuperman <2750556766@qq.com>
2023-05-23 23:39:12 +0800

    feat: docker config

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 
 // 创建数据库
 const db = mysql.createPool({
-  host: "127.0.0.1",
+  host: "electriccharge-mysqldb-1",
   user: "root",
   password: "123456789",
   database: "electric_charge",
--- dockerfile
+++ dockerfile
@@ -1,6 +1,6 @@
 FROM node:16-alpine
 ADD . /code
 WORKDIR /code
-RUN npm i --registry=https://registry.npmmirror.com/
+RUN npm i
 EXPOSE 5001
 CMD ["node", "./wechat.js"]
